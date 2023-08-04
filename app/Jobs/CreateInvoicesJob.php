a6aa8127a34ea801cd2fd0bbef1a318a2a95fdd4
Jeremymbugus <jeremymbuguz53@gmail.com>
2023-08-04 14:08:22 +0300

    new changes

--- app/Jobs/CreateInvoicesJob.php
+++ app/Jobs/CreateInvoicesJob.php
@@ -43,52 +43,57 @@ class CreateInvoicesJob implements ShouldQueue
      */
     public function handle()
     {
-        $company_id = $this->requestData['company_id'];
-        $environment =$this->requestData['environment'];
+        try{
+            $company_id = $this->requestData['company_id'];
+            $environment =$this->requestData['environment'];
 
-        $company_data = CompanyMaster::where('id', $company_id)->first();
+            $company_data = CompanyMaster::where('id', $company_id)->first();
 
-        $data = $this->requestData;
+            $data = $this->requestData;
 
-        if ($environment == 'sandbox') {
-            $account_number = $company_data->account_number . '-sb1';
-        } else {
-            $account_number = $company_data->account_number;
-        }
-        $existing_invoices = [];
-        $created_invoices = [];
-        $failed_invoices = [];
-        $failed_message = [];
-        foreach ($data['invoice'] as $data_return){
-            $invoice_number = $data_return['invoice_number'];
-            $invoice = $this->saritInvoiceController->findInvoice($company_id,$environment,$invoice_number);
-
-            if($invoice['message']->count > 0){
-                $existing_invoices =  $invoice_number;
+            if ($environment == 'sandbox') {
+                $account_number = $company_data->account_number . '-sb1';
             } else {
-                $url = "https://" . $account_number . ".restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=customscript_create_invoice&deploy=customdeploy_create_invoice";
-                $method = "POST";
-                $data = "";
-                $data = json_encode($data_return);
-                $send_request = $this->netsuite_connector->callRestApi($url, $method, $data, $company_data, $environment);
+                $account_number = $company_data->account_number;
+            }
+            $existing_invoices = [];
+            $created_invoices = [];
+            $failed_invoices = [];
+            $failed_message = [];
+            foreach ($data['invoice'] as $data_return){
+                $invoice_number = $data_return['invoice_number'];
+                $invoice = $this->saritInvoiceController->findInvoice($company_id,$environment,$invoice_number);
 
-                if ($send_request['statusCode'] != 200) {
-                    $data = $send_request['message'];
-                    $failed_invoices = ['invoice_number'=>$invoice_number,'message'=>$data];
-
+                if($invoice['message']->count > 0){
+                    $existing_invoices =  $invoice_number;
                 } else {
-                    $created_invoices = $invoice_number;
+                    $url = "https://" . $account_number . ".restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=customscript_create_invoice&deploy=customdeploy_create_invoice";
+                    $method = "POST";
+                    $data = "";
+                    $data = json_encode($data_return);
+                    $send_request = $this->netsuite_connector->callRestApi($url, $method, $data, $company_data, $environment);
 
+                    if ($send_request['statusCode'] != 200) {
+                        $data = $send_request['message'];
+                        $failed_invoices = ['invoice_number'=>$invoice_number,'message'=>$data];
+
+                    } else {
+                        $created_invoices = $invoice_number;
+
+                    }
                 }
             }
-        }
-        $message = ['posted_successfully'=>$created_invoices,
-            'existing'=>$existing_invoices,
-            'failed'=>$failed_invoices];
+            $message = ['posted_successfully'=>$created_invoices,
+                'existing'=>$existing_invoices,
+                'failed'=>$failed_invoices];
 
-        $response  = ['status'=>200, 'response'=>'completed','message'=>$message];
+            $response  = ['status'=>200, 'response'=>'completed','message'=>$message];
+        }catch (\Exception $ex){
+            $response  = ['status'=>500, 'response'=>'failed','message'=>$ex->getMessage()];
+        }
+
         $handler = fopen('response_sent_to_url'.$this->requestData['callback_url'].'_'.date('d_m_yyyy').'.txt','a');
-        fwrite($handler,json_encode($request->all()));
+        fwrite($handler,json_encode($response)));
         fclose($handler);
         //create a call to callback ul and return the response
 
