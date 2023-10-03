402d5653321ad30b25d9255c04bf0188a05123b1
Jeremymbugus <jeremymbuguz53@gmail.com>
2023-10-03 23:13:23 +0300

    sales

--- app/Http/Controllers/ReturnsController.php
+++ app/Http/Controllers/ReturnsController.php
@@ -24,12 +24,12 @@ class ReturnsController extends Controller
         $this->netsuite_connector = $netsuite_connector;
     }
 
-    public function postReturns(Request $request){
+    public function postReturnsOld(Request $request){
         try{
 
             $company_id = $request->company_id;
             $environment = $request->environment;
-            //return ($request->all());
+            return ($request->all());
 
             $company_data = CompanyMaster::where('id', $company_id)->first();
             // dd($company_id);
@@ -77,6 +77,77 @@ class ReturnsController extends Controller
         } catch (\Exception $ex) {
             return response()->json(['statusCode' => 300, 'response' => 'Something went wrong',
                 'message' => 'Error: '.$ex->getMessage().' File: '.$ex->getFile().' Line: '.$ex->getLine()]);
+        }
+    }
+
+    public function postReturns(Request $request){
+        try{
+            // return $request;
+            $company_id = $request->company_id;
+            $environment = $request->environment;
+
+            $company_data = CompanyMaster::where('id', $company_id)->first();
+            if($environment == 'sandbox'){
+                $account_number = $company_data->account_number.'-sb1';
+            }else{
+                $account_number = $company_data->account_number;
+            }
+            $received_data = $request->return;
+
+            foreach ($received_data as $payment){
+                $url = "https://".$account_number.".restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=customscript_create_return&deploy=customdeploy_create_return";
+                $method = "POST";
+                $data = "";
+                $data = json_encode($payment);
+                //return $url;
+                $send_request = $this->netsuite_connector->callRestApi($url, $method, $data, $company_data, $environment);
+                // return ($send_request);
+                if ($send_request['statusCode'] != 200) {
+                    return $send_request;
+                } else {
+                    $data = $send_request['message'];
+
+                    if($data->success){
+                        return ['statusCode' => 200, 'response' => 'success', 'message' => $data];
+                    }else{
+                        return ['statusCode' => 300, 'response' => 'error', 'message' => $data];
+                    }
+                }
+
+            }
+
+
+        } catch (\Exception $ex) {
+            return response()->json(['statusCode' => 300, 'response' => 'Something went wrong',
+                'message' => 'Error: '.$ex->getMessage().' File: '.$ex->getFile().' Line: '.$ex->getLine()]);
+        }
+    }
+
+    public function findReturn($company_id, $environment, $order_number)
+    {
+        try {
+            $company_data = CompanyMaster::where('id', $company_id)->first();
+            if ($environment == 'sandbox') {
+                $account_number = $company_data->account_number . '-sb1';
+            } else {
+                $account_number = $company_data->account_number;
+            }
+
+            $url = "https://".$account_number .".suitetalk.api.netsuite.com/services/rest/record/v1/returnAuthorization?q=custbody_nn_pa_posno+CONTAIN+".$order_number;
+            // return $url;
+            $method = "GET";
+            $data = "";
+            $data = json_decode($data);
+            $response = $this->netsuite_connector->callRestApi($url, $method, $data, $company_data, $environment);
+            if ($response['statusCode'] != 200) {
+                return $response;
+            } else {
+                $data = $response['message'];
+                return ['statusCode' => 200, 'response' => 'Success', 'message' => $data];
+            }
+        } catch (\Exception $ex) {
+            return response()->json(['statusCode' => 300, 'response' => 'Something went wrong',
+                'message' => 'Error: ' . $ex->getMessage() . ' File: ' . $ex->getFile() . ' Line: ' . $ex->getLine()]);
         }
     }
 
@@ -140,7 +211,7 @@ class ReturnsController extends Controller
 
     }
 
-    public function findReturn($company_id,$environment,$order_number){
+    public function findReturnOld($company_id,$environment,$order_number){
         try {
             $company_data = CompanyMaster::where('id', $company_id)->first();
             if($environment == 'sandbox'){
